178dd0d617ec1f6a049d7cf3bd4e36192d1acc67
dragoljub123 <dragoljub.miladinovic@bazarko.rs>
2024-03-20 13:11:55 +0100

    fin3

--- app/page.tsx
+++ app/page.tsx
@@ -79,6 +79,19 @@ export default async function Home() {
           </div>
         </div>
       </div>
+      <div className=" flex flex-col text-center justify-center  py-20 ">
+        <h1 className="text-5xl primarna font-bold mb-4">
+          Trade
+          <span className="narandzasta">
+            100+ Instruments <br></br>
+          </span>
+          with an Award-Winning Broker
+        </h1>
+        <p className="text-black mb-4">
+          Diversify your investment portfolio by trading CFDs <br></br>
+          on more than just Forex.
+        </p>
+      </div>
 
       <div className="flex gap-10 justify-center ">
         <Smalcard

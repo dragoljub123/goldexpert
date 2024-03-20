e1e9af3361cacc4bc4b7326c5596a923b394fd3e
dragoljub123 <dragoljub.miladinovic@bazarko.rs>
2024-03-20 12:50:17 +0100

    fin2

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,8 @@ import { Logo } from "./Logo";
 import Link from "next/link";
 import { FiMenu } from "react-icons/fi";
 import "../globals.css";
+import Image from "next/image";
+import Ellipse from "@/app/images/Ellipse.png";
 
 export const Navbar = () => {
   const navigation = [
@@ -15,7 +17,7 @@ export const Navbar = () => {
   ];
   return (
     <div className="w-full pozadina h-20   top-0 backdrop-blur-2xl transition-colors z-50">
-      <div className="max-w-screen-xl mx-auto flex items-center justify-between px-4 lg:px-0 h-full">
+      <div className="max-w-screen-xl mx-auto flex items-center justify-between  lg:px-0 h-full">
         <Logo title="Capitalrevo" className="text-black " />
         <div className="hidden md:inline-flex items-center gap-10 text-white hover:text-white duration-200">
           {navigation.map((item) => (
@@ -24,12 +26,13 @@ export const Navbar = () => {
               <span className="" />
             </Link>
           ))}
-          <button className=" text-white granica rounded-2xl  w-8/12">
+          <button className=" text-white granica rounded-2xl  w-20">
             Log in
           </button>
-          <button className="bg-green-500 text-white  rounded-2xl  w-8/12">
+          <button className="bg-green-500 text-white  rounded-2xl  w-20">
             Sing up
           </button>
+          <Image src={Ellipse} alt="slika" className=" object-contain" />
         </div>
 
         <div className="md:hidden">
--- app/page.tsx
+++ app/page.tsx
@@ -36,49 +36,58 @@ export default async function Home() {
 
   return (
     <div>
-      <div className="">
-        <Navbar />
-        <div className="flex items-center justify-center h-screen pozadina">
-          <div className="flex flex-col w-full md:w-1/2 bg-transparent rounded-lg p-20 ">
-            <h1 className="text-3xl text-white font-bold mb-4">
-              Trade forex and CFDs <br></br> with the world's #1 broker
-            </h1>
-            <p className="text-white mb-4">
-              Trade forex with unbeatable spreads, negative balance protection,
-              and award winning 24/7 support
-            </p>
-            <button className="bg-green-500 text-white px-4 py-2 rounded-2xl hover:bg-green-600 transition-colors duration-300 w-3/12">
-              Start trading
-            </button>
+      <div className="narandzasta2 ">
+        <div className="pozadina  ">
+          <Navbar />
+          <div className="flex items-center justify-center h-screen ">
+            <div className="flex flex-col w-full md:w-1/2 bg-transparent rounded-lg p-20 ">
+              <h1 className="text-5xl text-white font-bold mb-4">
+                Trade forex and CFDs <br></br> with the{" "}
+                <span className="narandzasta">
+                  world's #1 <br></br>broker
+                </span>
+              </h1>
+              <p className="text-white mb-4">
+                Trade forex with unbeatable spreads, negative balance<br></br>{" "}
+                protection, and award winning 24/7 support
+              </p>
+              <button className="bg-green-500 text-white px-4 py-2 rounded-2xl hover:bg-green-600 transition-colors duration-300 w-3/12">
+                Start trading
+              </button>
+            </div>
+            <div className="hidden md:flex md:w-1/2">
+              <Image
+                src={image23}
+                alt="slika"
+                className="max-h-screen object-contain"
+              />
+            </div>
           </div>
-          <div className="hidden md:flex md:w-1/2">
+          <div className="flex items-center justify-center   ">
+            <p className="text-white">Excellent &nbsp;</p>
             <Image
-              src={image23}
+              src={Stars}
+              alt="slika"
+              className="max-h-screen object-contain"
+            />
+            <p className="text-white"> &nbsp; 724 reviews on &nbsp; </p>
+            <Image
+              src={Frame515}
               alt="slika"
               className="max-h-screen object-contain"
             />
           </div>
         </div>
-        <div className="flex items-center justify-center  pozadina  ">
-          <p className="text-white">Excellent &nbsp;</p>
-          <Image
-            src={Stars}
-            alt="slika"
-            className="max-h-screen object-contain"
-          />
-          <p className="text-white"> &nbsp; 724 reviews on &nbsp; </p>
-          <Image
-            src={Frame515}
-            alt="slika"
-            className="max-h-screen object-contain"
-          />
-        </div>
       </div>
 
       <div className="flex gap-10 justify-center ">
-        <Smalcard imageSrc1="{circle-outline}" imageSrc2="" text="Forex" />
         <Smalcard
-          imageSrc1="/images/image1.jpg"
+          imageSrc1="./images/circleoutline.png"
+          imageSrc2="@\app\images\plus.png"
+          text="Forex"
+        />
+        <Smalcard
+          imageSrc1=""
           imageSrc2="@\app\images\plus.png"
           text="Text between pictures 1 and 2"
         />

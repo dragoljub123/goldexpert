9a183b3e9e9449511faaea751ad5e91036ffccaf
dragoljub123 <dragoljub.miladinovic@bazarko.rs>
2024-07-23 08:42:19 +0200

    navbarright side

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
   const navigation = [
     { title: "Home", href: "/" },
     { title: "Trading robots", href: "/Tradingrobots" },
-    { title: "About ", href: "/AboutUS" },
+    { title: "About", href: "/AboutUS" },
     { title: "Contact", href: "/ContactUs" },
   ];
   const [showLinks, setShowLinks] = useState(false);
@@ -22,23 +22,23 @@ export const Navbar = () => {
   const toggleLinks = () => {
     setShowLinks(!showLinks);
   };
+
   return (
-    <div className="w-full  h-16   z-50 ">
+    <div className="w-full h-16 z-50">
       <div className="max-w-screen-xl mx-auto flex items-center justify-between px-4 lg:px-0 h-full">
-        <Logo title="Capitalrevo" className="text-black " />
+        <Logo title="Capitalrevo" className="text-black" />
         <div className="hidden lg:inline-flex items-center gap-10 text-white">
           {navigation.map((item) => (
-            <Link key={item?.title} href={item?.href} className="text-sm  ">
-              {item?.title}
+            <Link key={item.title} href={item.href} className="text-sm">
+              {item.title}
             </Link>
           ))}
-
           <button className="bg-customBlue text-white px-12 py-1 rounded-3xl hover:bg-zlatna transition-colors duration-300 w-25">
             Get started
           </button>
         </div>
 
-        <div className="md:hidden ">
+        <div className="md:hidden">
           <button
             onClick={toggleLinks}
             className="text-white text-2xl focus:outline-none"
@@ -46,10 +46,16 @@ export const Navbar = () => {
             <FiMenu />
           </button>
           {showLinks && (
-            <div className="flex flex-col absolute top-16 right-4 bg-white  p-4 rounded shadow-lg ">
+            <div className="fixed top-0 right-0 w-64 h-full bg-white p-4 shadow-lg transition-transform transform translate-x-0">
+              <button
+                onClick={toggleLinks}
+                className="text-black text-2xl focus:outline-none mb-4"
+              >
+                &times;
+              </button>
               {navigation.map((item) => (
-                <Link key={item?.title} href={item?.href}>
-                  {item?.title}
+                <Link key={item.title} href={item.href} className="block mb-4">
+                  {item.title}
                 </Link>
               ))}
             </div>
